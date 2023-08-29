7ce2d429baf82e330d424ba6cd93e69aacffd2f1
lutes <robert.lutes@pnnl.gov>
2023-08-29 10:48:55 -0700

    Update data_utils file for AEMS consistency.

--- EnergyEfficiency/OptimalStart/optimal_start/data_utils.py
+++ EnergyEfficiency/OptimalStart/optimal_start/data_utils.py
@@ -1,3 +1,44 @@
+"""
+Copyright (c) 2023, Battelle Memorial Institute
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project.
+This material was prepared as an account of work sponsored by an agency of the
+United States Government. Neither the United States Government nor the United
+States Department of Energy, nor Battelle, nor any of their employees, nor any
+jurisdiction or organization that has cooperated in th.e development of these
+materials, makes any warranty, express or implied, or assumes any legal
+liability or responsibility for the accuracy, completeness, or usefulness or
+any information, apparatus, product, software, or process disclosed, or
+represents that its use would not infringe privately owned rights.
+Reference herein to any specific commercial product, process, or service by
+trade name, trademark, manufacturer, or otherwise does not necessarily
+constitute or imply its endorsement, recommendation, or favoring by the
+United States Government or any agency thereof, or Battelle Memorial Institute.
+The views and opinions of authors expressed herein do not necessarily state or
+reflect those of the United States Government or any agency thereof.
+PACIFIC NORTHWEST NATIONAL LABORATORY
+operated by BATTELLE for the UNITED STATES DEPARTMENT OF ENERGY
+under Contract DE-AC05-76RL01830
+"""
 import os
 import logging
 import pandas as pd
@@ -31,8 +72,8 @@ class Data:
         _log.debug("Data file: {}".format(data_file))
         if os.path.isfile(data_file):
             try:
-                self.df = pd.read_csv(data_file)
-                self.df.set_index('timeindex')
+                self.df = pd.read_csv(data_file, index_col='ts')
+                self.df.index = pd.to_datetime(self.df.index).apply(self.assign_local_tz)
             except Exception as ex:
                 _log.debug("No previous dataframe object: %s", ex)
 
@@ -91,10 +132,11 @@ class Data:
         if stored_data:
             stored_data['ts'] = [current_time]
             df = pd.DataFrame.from_dict(stored_data)
-            df['timeindex'] = df['ts']
-            df = df.set_index(df['timeindex'])
+            df['ts'] = pd.to_datetime(df['ts'])
+            df.set_index(df['ts'], inplace=True)
             if self.df is not None:
                 self.df = pd.concat([self.df, df], axis=0, ignore_index=False)
+                self.df = self.df.drop(columns=['ts'])
             else:
                 self.df = df
             data_path = self.data_path + "/data_{}.csv".format(self.tag)
